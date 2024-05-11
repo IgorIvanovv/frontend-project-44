f15f55b25378e531cda46e2db47d1c47fabadbb4
Igor Ivanov <iia02@yandex.ru>
2024-05-11 07:47:47 +0300

    connect eslint

--- Makefile
+++ Makefile
@@ -5,3 +5,5 @@ start:
 	node bin/brain-games.js
 publish:
 	npm publish --dry-run
+lint:
+	npx eslint .
--- src/cli.js
+++ src/cli.js
@@ -1,4 +1,5 @@
 import readlineSync from 'readline-sync';
+
 const greeting = () => {
   const name = readlineSync.question('May I have your name?');
   return `Hello, ${name}!`;
